bffbe7d5d9968e19f63b0b5433cf95a1e60dcce7
akash-1712 <akashbansalab01@gmail.com>
2021-06-16 15:40:52 +0530

    JavaScript

--- js/index.js
+++ js/index.js
@@ -5,6 +5,38 @@ const btn_form = document.querySelector(".btn-form");
 const error_message = document.querySelector(".error-message");
 const form_inputs = document.querySelectorAll(".form-input");
 
+// const passwordCheck = function (pass) {
+//   var lowerCaseLetters = /[a-z]/g;
+
+// };
+
+const emailCheck = function (email) {
+  const checker =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  return email.match(checker);
+};
+
+const addErrorMessage = function (val, html) {
+  val.insertAdjacentHTML("afterend", html);
+  val.style.border = "2px solid hsl(246, 25%, 77%)";
+};
+const errorRemove = function (val, parent, child) {
+  val
+    .closest(parent)
+    ?.querySelectorAll(child)
+    ?.forEach((arr) => {
+      arr?.remove();
+    });
+};
+
+const removeErrorMessage = function (val) {
+  val.style.border = "2px solid hsl(248, 32%, 49%)";
+  val.style.color = " rgba(#000000, 0.4)";
+  errorRemove(val, ".error", ".error-icon");
+  errorRemove(val, ".error", ".error-message");
+  val.blur();
+};
+
 const giveError = function (event) {
   event.preventDefault();
   form_inputs.forEach((val) => {
@@ -16,15 +48,21 @@ const giveError = function (event) {
                   />
         <p class="error-message">${val.dataset.error} cannot be empty.</p>
         `;
-    if (val.value == "") {
-      val.insertAdjacentHTML("afterend", html);
-      val.style.border = "2px solid hsl(246, 25%, 77%)";
+    if (val.value == "" && !val.classList.contains("check-email")) {
+      // val.insertAdjacentHTML("afterend", html);
+      // val.style.border = "2px solid hsl(246, 25%, 77%)";
+      addErrorMessage(val, html);
+    } else if (val.classList.contains("check-email")) {
+      !emailCheck(val.value)
+        ? addErrorMessage(val, html)
+        : removeErrorMessage(val);
     } else {
-      val.style.border = "2px solid hsl(248, 32%, 49%)";
-      val.style.color = " rgba(#000000, 0.4)";
-      val.closest(".error")?.querySelector(".error-icon")?.remove();
-      val.closest(".error")?.querySelector(".error-message")?.remove();
-      val.blur();
+      // val.style.border = "2px solid hsl(248, 32%, 49%)";
+      // val.style.color = " rgba(#000000, 0.4)";
+      // val.closest(".error")?.querySelector(".error-icon")?.remove();
+      // val.closest(".error")?.querySelector(".error-message")?.remove();
+      // val.blur();
+      removeErrorMessage(val);
     }
   });
 };
